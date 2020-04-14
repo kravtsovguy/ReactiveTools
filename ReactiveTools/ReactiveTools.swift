26da3726c0a10dd7f613f696c06618cebfcaec26
Matvey Kravtsov <kravtsovguy@gmail.com>
2020-04-15 00:20:59 +0300

    remove code

--- ReactiveTools/ReactiveTools.swift
+++ ReactiveTools/ReactiveTools.swift
@@ -5,13 +5,3 @@
 //  Created by Matvey Kravtsov on 14.04.2020.
 //  Copyright © 2020 Matvey Kravtsov. All rights reserved.
 //
-
-import RxSwift
-import RxCocoa
-import RxRelay
-
-public final class ReactiveTools {
-  let observable = Observable.just(Void())
-  let driver = Driver.just(Void())
-  let relay = PublishRelay<Void>()
-}
