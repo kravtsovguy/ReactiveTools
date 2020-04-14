139fa5fc5393d5609804e41a1e5e6a65d9812e3f
Matvey Kravtsov <kravtsovguy@gmail.com>
2020-04-14 04:45:41 +0300

    up

--- Package.swift
+++ Package.swift
@@ -24,8 +24,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
           name: "ReactiveTools",
-          dependencies: ["RxSwift"],
+          dependencies: ["RxSwift", "RxCocoa"],
           path: "ReactiveTools"
-        ),
+        )
     ]
 )
--- ReactiveTools/ReactiveTools.swift
+++ ReactiveTools/ReactiveTools.swift
@@ -7,3 +7,4 @@
 //
 
 import RxSwift
+import RxCocoa
