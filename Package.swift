e11face9cc2673aa49676ff8aad188346674acd0
Matvey Kravtsov <kravtsovguy@gmail.com>
2020-04-14 03:38:34 +0300

    added dynamic lib

--- Package.swift
+++ Package.swift
@@ -8,8 +8,12 @@ let package = Package(
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "ReactiveTools",
-            targets: ["ReactiveTools"]),
+          name: "ReactiveTools",
+          targets: ["ReactiveTools"]),
+        .library(
+          name: "ReactiveTools-Dynamic",
+          type: .dynamic,
+          targets: ["ReactiveTools"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -19,7 +23,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "ReactiveTools",
-            dependencies: []),
+          name: "ReactiveTools",
+          dependencies: []),
     ]
 )
