de1074bd81be4f0286416b43aba90a8e2dff2151
Matvey Kravtsov <kravtsovguy@gmail.com>
2020-04-14 03:49:27 +0300

    added project

---
+++ ReactiveTools/ReactiveTools.h
@@ -0,0 +1,19 @@
+//
+//  ReactiveTools.h
+//  ReactiveTools
+//
+//  Created by Matvey Kravtsov on 14.04.2020.
+//  Copyright © 2020 Matvey Kravtsov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ReactiveTools.
+FOUNDATION_EXPORT double ReactiveToolsVersionNumber;
+
+//! Project version string for ReactiveTools.
+FOUNDATION_EXPORT const unsigned char ReactiveToolsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ReactiveTools/PublicHeader.h>
+
+
